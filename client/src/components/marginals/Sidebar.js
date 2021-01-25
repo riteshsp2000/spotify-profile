14dd8960e41b30370c60aa312936b794234b97e0
Ritesh Patil <riteshsp2000@gmail.com>
2021-01-25 23:28:57 +0530

    Add routing for the menu

--- client/src/components/marginals/Sidebar.js
+++ client/src/components/marginals/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
 export default function MiniDrawer({ children }) {
   const classes = useStyles();
@@ -9,11 +10,26 @@ export default function MiniDrawer({ children }) {
       <div className={classes.container}>
         <img className={classes.logo} src='/favicon.png' alt='spotify logo' />
 
-        <i className={`${classes.icon} fas fa-2x fa-user-alt`} />
-        <i className={`${classes.icon} fas fa-2x fa-music`} />
-        <i className={`${classes.icon} fas fa-2x fa-microphone`} />
-        <i className={`${classes.icon} fas fa-2x fa-history`} />
-        <i className={`${classes.icon} fas fa-2x fa-play`} />
+        <Link className={classes.link} to='/'>
+          <i className={`${classes.icon} fas fa-2x fa-user-alt`} />
+          <h3 className={classes.menuName}>Profile</h3>
+        </Link>
+        <Link className={classes.link} to='/tracks'>
+          <i className={`${classes.icon} fas fa-2x fa-music`} />
+          <h3 className={classes.menuName}>Tracks</h3>
+        </Link>
+        <Link className={classes.link} to='/artists'>
+          <i className={`${classes.icon} fas fa-2x fa-microphone`} />
+          <h3 className={classes.menuName}>Artists</h3>
+        </Link>
+        <Link className={classes.link} to='/recent'>
+          <i className={`${classes.icon} fas fa-2x fa-history`} />
+          <h3 className={classes.menuName}>Recent</h3>
+        </Link>
+        <Link className={classes.link} to='/playlist'>
+          <i className={`${classes.icon} fas fa-2x fa-play`} />
+          <h3 className={classes.menuName}>Playlist</h3>
+        </Link>
       </div>
     </div>
   );
@@ -27,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#040306',
     [theme.breakpoints.down('sm')]: {
       width: '100%',
-      minHeight: '70px',
+      minHeight: '80px',
       bottom: 0,
     },
     [theme.breakpoints.up('sm')]: {
@@ -59,17 +75,27 @@ const useStyles = makeStyles((theme) => ({
       display: 'none',
     },
   },
-  icon: {
+  link: {
     margin: '20px 0px',
-    color: '#DCDCDC',
     [theme.breakpoints.down('xs')]: {
       margin: 0,
-      padding: '15px 10px',
+      padding: '15px 10px 10px 10px',
       width: '25%',
-      height: '100%',
+      height: '80px',
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
     },
+  },
+  icon: {
+    color: '#DCDCDC',
+  },
+  menuName: {
+    color: '#DCDCDC',
+    fontSize: 10,
+    marginTop: 5,
+    marginBottom: 0,
+    fontFamily: "'Montserrat', sans-serif",
   },
 }));
