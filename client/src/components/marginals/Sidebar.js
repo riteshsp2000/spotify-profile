c695f93c3be33f1a936b6a318bdd6a5964bdda49
Ritesh Patil <riteshsp2000@gmail.com>
2021-01-25 23:42:41 +0530

    Add styling for active menu

--- client/src/components/marginals/Sidebar.js
+++ client/src/components/marginals/Sidebar.js
@@ -1,32 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 
 export default function MiniDrawer({ children }) {
   const classes = useStyles();
+  const [active, setActive] = useState(0);
 
   return (
     <div className={classes.root}>
       <div className={classes.container}>
         <img className={classes.logo} src='/favicon.png' alt='spotify logo' />
 
-        <Link className={classes.link} to='/'>
+        <Link
+          className={
+            active === 0 ? `${classes.link} ${classes.active}` : classes.link
+          }
+          to='/'
+          onClick={() => setActive(0)}
+        >
           <i className={`${classes.icon} fas fa-2x fa-user-alt`} />
           <h3 className={classes.menuName}>Profile</h3>
         </Link>
-        <Link className={classes.link} to='/tracks'>
+        <Link
+          className={
+            active === 1 ? `${classes.link} ${classes.active}` : classes.link
+          }
+          to='/tracks'
+          onClick={() => setActive(1)}
+        >
           <i className={`${classes.icon} fas fa-2x fa-music`} />
           <h3 className={classes.menuName}>Tracks</h3>
         </Link>
-        <Link className={classes.link} to='/artists'>
+        <Link
+          className={
+            active === 2 ? `${classes.link} ${classes.active}` : classes.link
+          }
+          to='/artists'
+          onClick={() => setActive(2)}
+        >
           <i className={`${classes.icon} fas fa-2x fa-microphone`} />
           <h3 className={classes.menuName}>Artists</h3>
         </Link>
-        <Link className={classes.link} to='/recent'>
+        <Link
+          className={
+            active === 3 ? `${classes.link} ${classes.active}` : classes.link
+          }
+          to='/recent'
+          onClick={() => setActive(3)}
+        >
           <i className={`${classes.icon} fas fa-2x fa-history`} />
           <h3 className={classes.menuName}>Recent</h3>
         </Link>
-        <Link className={classes.link} to='/playlist'>
+        <Link
+          className={
+            active === 4 ? `${classes.link} ${classes.active}` : classes.link
+          }
+          to='/playlist'
+          onClick={() => setActive(4)}
+        >
           <i className={`${classes.icon} fas fa-2x fa-play`} />
           <h3 className={classes.menuName}>Playlist</h3>
         </Link>
@@ -76,16 +107,26 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   link: {
-    margin: '20px 0px',
+    margin: '0px',
+    width: '100%',
+    height: '80px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     [theme.breakpoints.down('xs')]: {
       margin: 0,
       padding: '15px 10px 10px 10px',
       width: '25%',
       height: '80px',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
+    },
+  },
+  active: {
+    backgroundColor: '#181818',
+    borderLeft: '5px solid rgb(30, 215, 96)',
+    [theme.breakpoints.down('xs')]: {
+      borderLeft: 0,
+      borderTop: '3px solid rgb(30, 215, 96)',
     },
   },
   icon: {
